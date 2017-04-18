17fca48978447f8bcf87e29c87b7b3f05d9a17bd
zizi04 <disguy3939@naver.com>
2017-04-18 15:45:23 +0900

    자바2
    자바두번째소스추

---
+++ helloprogramming.java
@@ -0,0 +1,16 @@
+package program;
+
+
+
+public class helloprogramming {
+
+
+}         //필드선언
+
+ public string fd = "-- 필드"
+static string .fd = "-- 메소드";
+ //자바 프로그램을 실행하는 main(string[] args) {
+ system.out.println("자바 클래스의 구조");
+ write(fd);
+ write(md);
+ 
